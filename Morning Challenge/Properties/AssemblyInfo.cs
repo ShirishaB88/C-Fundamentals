96b3bd944fd6e08090cd94b0c2aa354f03f26008
ShirishaB88 <shirisha46@gmail.com>
2021-03-10 16:24:42 -0500

    challenge inint

---
+++ Morning Challenge/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Morning Challenge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Morning Challenge")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("629f2cb3-8afa-4f06-b970-b36d7fe72c43")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Morning Challenge/conditionalAndLoop.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Morning_Challenge
+{
+    [TestClass]
+    public class conditionalAndLoop
+    {
+        string challenge = "Supercalifragilisticexpialidocious";
+        [TestMethod]
+        public void PrinteveryLetter()
+        {
+            foreach (char letter in challenge)
+            {
+                Console.WriteLine(letter);
+
+            }
+
+
+        }
+
+        [TestMethod]
+        public void step2()
+        {
+            foreach (char letter in challenge)
+                if (letter == 'i')
+            {
+                Console.WriteLine("It's an i");
+            }
+            else
+            {
+                Console.WriteLine("It's not an i");
+            }
+        }
+
+        [TestMethod]
+        public void step3()
+        {
+            int lengthOfString = challenge.Length;
+            Console.WriteLine("Number of Characters in string is :" + lengthOfString);
+
+            Console.WriteLine(challenge.Length);
+        }
+
+        [TestMethod]
+        public void PrintL()
+        {
+            foreach (char letter in challenge)
+            {
+                if(letter == 'l')
+                {
+                    Console.WriteLine('L');
+                }
+            }
+        }
+    }
+}
+
